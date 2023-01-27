7c6e12e92c8fadddfd172cf3d656681b813c016e
Guosong Yang <eegsyang@gmail.com>
2023-01-27 00:55:18 -0500

    Menu js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-﻿
+function navClick() {
+	var x = document.getElementById("navList");
+	if (x.className === "nav") {
+		x.className += " responsive";
+	} else {
+		x.className = "nav";
+	}
+}
+
+$(function() {
+	$('#navList li').not('.menu').click(function() {
+		$('#navList li').removeClass('current');
+		$(this).addClass('current');
+	});
+});
